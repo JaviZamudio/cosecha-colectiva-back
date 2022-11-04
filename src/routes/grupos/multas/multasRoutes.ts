4f6b34aa91fbc83f8dc227fb9606318fdaa36e06
Ale De Alvino <alejandra.dealvino.melgoza@gmail.com>
2022-11-04 11:42:57 -0600

    get multas lista

--- src/routes/grupos/multas/multasRoutes.ts
+++ src/routes/grupos/multas/multasRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { pagar_multas } from "../../../controllers/multas_control";
+import { pagar_multas, get_multas_activas_por_grupo } from "../../../controllers/multas_control";
 import { authAdmin } from "../../../middleware/auth";
 
 // Router empezando en /api/grupos/:Grupo_id/multas
 const router = Router({ mergeParams: true });
+
+//Ver multas por grupo
+router.get("/", authAdmin, get_multas_activas_por_grupo);
 
 // Pagar multas
 router.patch("/", authAdmin, pagar_multas);
