647ea53e2fb4135ec7a14f2131d85b6e1695ea3a
JaviZamudio <javier.elihu100@gmail.com>
2023-05-17 14:42:12 -0600

    Cambio de entidades a typescript. Update de ruta para el front

--- src/controllers/prestamos_control.ts
+++ src/controllers/prestamos_control.ts
@@ -228,6 +228,7 @@ export const pagar_prestamos = async (req: AdminRequest<PayloadPagarPrestamos>, 
     }
 }
 
+// resBody = { Prestamo_id: number, Adeudo_prestamo: number, Adeudo_interes: number, Sesiones_restantes: number }
 export const get_prestamos_nopagados_socio = async (req: AdminRequest<Grupo>, res) => {
     const Grupo_id = Number(req.id_grupo_actual);
     const Socio_id = Number(req.params.Socio_id);
@@ -237,7 +238,13 @@ export const get_prestamos_nopagados_socio = async (req: AdminRequest<Grupo>, re
         const sesionActual = await obtenerSesionActual(Grupo_id);
 
         //Total de prestamos no pagados de un socio
-        let query = "SELECT prestamos.Prestamo_id, prestamos.Fecha_inicial AS Fecha, (prestamos.Interes_generado - prestamos.Interes_pagado) AS Interes, prestamos.Monto_prestamo AS Total, prestamos.Monto_pagado AS Pagado FROM prestamos JOIN sesiones ON prestamos.Sesion_id = sesiones.Sesion_id WHERE Socio_id = ? AND Estatus_prestamo = 0 AND sesiones.Grupo_id = ?;";
+        // let query = "SELECT prestamos.Prestamo_id, prestamos.Fecha_inicial AS Fecha, (prestamos.Interes_generado - prestamos.Interes_pagado) AS Interes, prestamos.Monto_prestamo AS Total, prestamos.Monto_pagado AS Pagado FROM prestamos JOIN sesiones ON prestamos.Sesion_id = sesiones.Sesion_id WHERE Socio_id = ? AND Estatus_prestamo = 0 AND sesiones.Grupo_id = ?;";
+        let query = `
+            SELECT prestamos.Prestamo_id, prestamos.Monto_prestamo - prestamos.Monto_pagado AS Adeudo_prestamo, prestamos.Interes_generado - prestamos.Interes_pagado AS Adeudo_interes, prestamos.Sesiones_restantes
+            FROM prestamos
+            JOIN sesiones ON prestamos.Sesion_id = sesiones.Sesion_id
+            WHERE Socio_id = ? AND Estatus_prestamo = 0 AND sesiones.Grupo_id = ?
+        `
         const [prestamos] = await db.query(query, [Socio_id, Grupo_id]);
 
         return res.status(200).json({ code: 200, message: 'Prestamos obtenidos', data: prestamos });
